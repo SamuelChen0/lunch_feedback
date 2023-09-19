3cc87a13ad245e62b839beace4c88bae63c801a5
Jefferson <jeffersonrenchin@gmail.com>
2023-09-19 11:22:08 +0800

    counter on button

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Initialize the click count to zero
+click_count = 0
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', click_count=click_count)
+
+@app.route('/click', methods=['POST'])
+def click():
+    global click_count
+    click_count += 1
+    return 'success'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
