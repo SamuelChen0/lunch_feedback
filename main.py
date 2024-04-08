43b60cf1e6fadab8227d2bee14af7b0799b2a9d6
rentsdue <hoganwong88@gmail.com>
2024-04-08 16:31:29 +0800

    Adding game

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def run_game():
+    java_command = [
+        'C:\\Program Files\\Eclipse Adoptium\\jdk-21.0.2.13-hotspot\\bin\\java.exe',
+        '-XX:+ShowCodeDetailsInExceptionMessages',
+        '-cp',
+        'C:\\Users\\admin\\PlatformGameProject\\bin',
+        'main.java.com.example.Main'
+    ]
+
+    subprocess.run(java_command, shell=True)
+
+    return 'Java game started!'
+# def index():
+#     return render_template('index.html')
+
+#@app.route('/rungame')
 
 if __name__ == '__main__':
     app.run(debug=True)
