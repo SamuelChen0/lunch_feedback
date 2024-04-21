231b62f7fbbe5218ffbaa8ff7fbe8c904f6ba8a7
Hogan <hoganwong88@gmail.com>
2024-04-21 21:19:33 +0800

    almost fully operational

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import subprocess
 import threading
 import time
@@ -6,7 +6,6 @@ import time
 app = Flask(__name__)
 
 game_process = None
-last_interaction_time = None
 
 import subprocess
 
@@ -15,13 +14,13 @@ def run_game():
     # This java command needs to be changed since it is different for every single device
     java_command = [
     # Java Executable Path
-    r'C:\Program Files\Eclipse Adoptium\jdk-21.0.2.13-hotspot\bin\java.exe',
+       r'C:\Program Files\Java\jdk-21\bin\java.exe',
     # Java VM Option
     '-XX:+ShowCodeDetailsInExceptionMessages',
     # Classpath Option
     '-cp',
     # Classpath Value
-    r'C:\Users\admin\PlatformGameProject\bin',
+    r'C:\Users\alfee\PlatformGameProject\bin',
     # Main Class to Run
     'main.java.com.example.Main'
 ]
@@ -29,15 +28,9 @@ def run_game():
     game_process = subprocess.Popen(java_command)
 
 def close_game_after_timeout():
-    global game_process, last_interaction_time
-    while True:
-        if last_interaction_time is not None:
-            time_diff = time.time() - last_interaction_time
-            if time_diff >= 10:  # If there's no interaction for 10 seconds
-                if game_process and game_process.poll() is None:  # If the game process is still running
-                    game_process.terminate()  # Terminate the game process
-                last_interaction_time = None  # Reset last_interaction_time
-        time.sleep(1)
+    time.sleep(60)  # Change this to 60 seconds later
+    if game_process and game_process.poll() is None:  # If the game process is still running
+        game_process.terminate()  # Terminate the game process
 
 @app.route('/')
 def index():
@@ -57,12 +50,6 @@ def run_game_route():
     print("Game is running")
 
     return 'Java game started!'
-
-@app.route('/interaction', methods=['POST'])
-def record_interaction():
-    global last_interaction_time
-    last_interaction_time = time.time()
-    return 'Interaction recorded!'
 
 if __name__ == '__main__':
     app.run(debug=True)
